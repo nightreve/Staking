0da4b4c82c5247b7ccebd43864cc9f816866c110
nightreve <nightreve@hotmail.co.uk>
2022-05-07 23:01:16 +0100

    Update Testd.sol

--- Testd.sol
+++ Testd.sol
@@ -1,8 +1,9 @@
+
 // SPDX-License-Identifier: MIT
 
 pragma solidity >=0.8.9 <0.9.0;
 
-import 'https://github.com/nightreve/Staking/blob/main/ERC721A.sol';
+import 'erc721a/contracts/ERC721A.sol';
 import '@openzeppelin/contracts/access/Ownable.sol';
 import '@openzeppelin/contracts/utils/cryptography/MerkleProof.sol';
 import '@openzeppelin/contracts/security/ReentrancyGuard.sol';
