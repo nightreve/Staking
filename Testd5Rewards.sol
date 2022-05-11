e1506d5ada3e20993fe7b4f8884f08c7db2f23be
nightreve <nightreve@hotmail.co.uk>
2022-05-11 18:46:32 +0100

    Update Testd5Rewards.sol

--- Testd5Rewards.sol
+++ Testd5Rewards.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT LICENSE
 
 pragma solidity ^0.8.4;
-import "@openzeppelin/contracts/access/Ownable.sol";
+//import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
