db6f6ffdde50255ace186fde14ee0da5887ddc9f
nightreve <nightreve@hotmail.co.uk>
2022-05-11 19:37:01 +0100

    Update and rename Testd5Contract.sol to Testd6Contract.sol

--- Testd6Contract.sol
+++ Testd6Contract.sol
@@ -1,37 +1,4 @@
-/**
- *Submitted for verification at Etherscan.io on 2021-12-08
-*/
-
-/**
- *Submitted for verification at Etherscan.io on 2021-11-01
-*/
-
-// SPDX-License-Identifier: GPL-3.0
-
-// File: @openzeppelin/contracts/utils/Context.sol
-
-pragma solidity ^0.8.0;
-
-/**
- * @dev Provides information about the current execution context, including the
- * sender of the transaction and its data. While these are generally available
- * via msg.sender and msg.data, they should not be accessed in such a direct
- * manner, since when dealing with meta-transactions the account sending and
- * paying for execution may not be the actual sender (as far as an application
- * is concerned).
- *
- * This contract is only required for intermediate, library-like contracts.
- */
-abstract contract Context {
-    function _msgSender() internal view virtual returns (address) {
-        return msg.sender;
-    }
-
-    function _msgData() internal view virtual returns (bytes calldata) {
-        return msg.data;
-    }
-}
-
+// SPDX-License-Identifier: MIT LICENSE
 // File: @openzeppelin/contracts/utils/introspection/IERC165.sol
 pragma solidity ^0.8.0;
 
@@ -604,6 +571,8 @@ interface IERC721Receiver {
 
 // File: @openzeppelin/contracts/token/ERC721/ERC721.sol
 pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/utils/Context.sol";
 /**
  * @dev Implementation of https://eips.ethereum.org/EIPS/eip-721[ERC721] Non-Fungible Token Standard, including
  * the Metadata extension, but not including the Enumerable extension, which is available separately as
@@ -1168,82 +1137,11 @@ abstract contract ERC721Enumerable is ERC721, IERC721Enumerable {
     }
 }
 
-// File: @openzeppelin/contracts/access/Ownable.sol
-
-
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 pragma solidity ^0.8.0;
 
-
-/**
- * @dev Contract module which provides a basic access control mechanism, where
- * there is an account (an owner) that can be granted exclusive access to
- * specific functions.
- *
- * By default, the owner account will be the one that deploys the contract. This
- * can later be changed with {transferOwnership}.
- *
- * This module is used through inheritance. It will make available the modifier
- * `onlyOwner`, which can be applied to your functions to restrict their use to
- * the owner.
- */
-abstract contract Ownable is Context {
-    address private _owner;
-
-    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
-
-    /**
-     * @dev Initializes the contract setting the deployer as the initial owner.
-     */
-    constructor() {
-        _setOwner(_msgSender());
-    }
-
-    /**
-     * @dev Returns the address of the current owner.
-     */
-    function owner() public view virtual returns (address) {
-        return _owner;
-    }
-
-    /**
-     * @dev Throws if called by any account other than the owner.
-     */
-    modifier onlyOwner() {
-        require(owner() == _msgSender(), "Ownable: caller is not the owner");
-        _;
-    }
-
-    /**
-     * @dev Leaves the contract without owner. It will not be possible to call
-     * `onlyOwner` functions anymore. Can only be called by the current owner.
-     *
-     * NOTE: Renouncing ownership will leave the contract without an owner,
-     * thereby removing any functionality that is only available to the owner.
-     */
-    function renounceOwnership() public virtual onlyOwner {
-        _setOwner(address(0));
-    }
-
-    /**
-     * @dev Transfers ownership of the contract to a new account (`newOwner`).
-     * Can only be called by the current owner.
-     */
-    function transferOwnership(address newOwner) public virtual onlyOwner {
-        require(newOwner != address(0), "Ownable: new owner is the zero address");
-        _setOwner(newOwner);
-    }
-
-    function _setOwner(address newOwner) private {
-        address oldOwner = _owner;
-        _owner = newOwner;
-        emit OwnershipTransferred(oldOwner, newOwner);
-    }
-}
-
-pragma solidity ^0.8.0;
-
-contract Testd5 is ERC721Enumerable, Ownable {
+contract Testd6 is ERC721Enumerable, Ownable {
     using Strings for uint256;
     string public baseURI;
     string public baseExtension = ".json";
@@ -1252,7 +1150,7 @@ contract Testd5 is ERC721Enumerable, Ownable {
     uint256 public maxMintAmount = 5;
     bool public paused = false;
 
-    constructor() ERC721("Testd Collection", "TD5") {}
+    constructor() ERC721("Testd6 NFT Collection", "TD6") {}
         // internal
         function _baseURI() internal view virtual override returns (string memory) {
         return "ipfs://QmfBpUqG6xpQ6kiswqWUYhvLi5ypnW2fW7spPJcLjvrqg7/";
@@ -1267,7 +1165,7 @@ contract Testd5 is ERC721Enumerable, Ownable {
             require(supply + _mintAmount <= maxSupply);
             
             if (msg.sender != owner()) {
-            require(msg.value == cost * _mintAmount, "Need to send 0.0008 ether!");
+            require(msg.value == cost * _mintAmount, "Need to send 0.08 ether!");
             }
             
             for (uint256 i = 1; i <= _mintAmount; i++) {
